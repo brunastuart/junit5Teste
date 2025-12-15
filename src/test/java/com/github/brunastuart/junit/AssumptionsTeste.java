5cb3e2b3fb9bd0a3d72f32e70d1e6d36e8e3cd61
brunastuart <deasbruna@gmail.com>
2025-12-15 03:57:01 -0300

    testagem assumptions

--- src/test/java/com/github/brunastuart/junit/AssumptionsTeste.java
+++ src/test/java/com/github/brunastuart/junit/AssumptionsTeste.java
@@ -7,8 +7,18 @@ import org.junit.jupiter.api.Test;
 public class AssumptionsTeste {
 
     @Test
-    void validarAlgoSomenteNoUsuarioWillyan() {
-        Assumptions.assumeFalse("root".equals(System.getenv("USER")));
+    void validarAlgoSomenteNoUsuario() {
+        Assumptions.assumeTrue("bruna".equals (System.getenv("USERNAME")));
+        //Assumptions.assumeFalse("root".equals(System.getenv("USER")));
         Assertions.assertEquals(10, 5 + 5);
+        System.out.println("Teste executado para o usuário: bruna;
+
+        /*
+        Para descobrir o nome da variável de ambiente do seu usuário, rode o código abaixo:
+        String userVariavel = System.getenv("USER");
+        String userNomeVariavel = System.getenv("USERNAME");
+        System.out.println("USER: " + userVariavel);
+        System.out.println("USERNAME: " + userNomeVariavel);
+        */
     }
 }
